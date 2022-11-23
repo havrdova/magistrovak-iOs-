7613b39080027d1410a0ca4b3c169c529e8abb02
Lucie Havrdová <havrdova1lucie@gmail.com>
2022-11-23 19:58:08 +0100

    Programme init

--- magistrovak/magistrovak/Programme/ProgrammeCore.swift
+++ magistrovak/magistrovak/Programme/ProgrammeCore.swift
@@ -1,5 +1,30 @@
+import ComposableArchitecture
 import Foundation
 
 // MARK: - Programme Reducer
 
+struct Programme: ReducerProtocol {
+    struct State: Equatable {
 
+    }
+
+    enum Action: Equatable {
+        case onAppear
+        case dataLoaded
+    }
+
+    var body: some ReducerProtocol<State, Action> {
+        Reduce { state, action in
+            switch action {
+            case .onAppear:
+                return .none
+
+            case .dataLoaded:
+                return .none
+                
+            }
+        }
+    }
+
+
+}
--- magistrovak/magistrovak/Programme/ProgrammeView.swift
+++ magistrovak/magistrovak/Programme/ProgrammeView.swift
@@ -1,3 +1,4 @@
+import ComposableArchitecture
 import SwiftUI
 
 // MARK: - Programme View
