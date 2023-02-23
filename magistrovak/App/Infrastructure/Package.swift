092b8022744c799d34a351e0a67482e9506e628b
Lucie Havrdová <havrdova1lucie@gmail.com>
2023-02-23 14:50:01 +0100

    CoreToolkit, UIToolkit created

--- magistrovak/App/Infrastructure/Package.swift
+++ magistrovak/App/Infrastructure/Package.swift
@@ -9,8 +9,13 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "Infrastructure",
-            targets: ["Infrastructure"]),
+            name: "CoreToolkit",
+            targets: ["CoreToolkit"]
+        ),
+        .library(
+            name: "UIToolkit",
+            targets: ["UIToolkit"]
+        )
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -19,11 +24,18 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
+
+        // MARK: - CoreToolKit Target
+
         .target(
-            name: "Infrastructure",
+            name: "CoreToolkit",
             dependencies: []),
-        .testTarget(
-            name: "InfrastructureTests",
-            dependencies: ["Infrastructure"]),
+
+        // MARK: - UIToolkit Target
+
+        .target(
+            name: "UIToolkit",
+            dependencies: []
+        )
     ]
 )
