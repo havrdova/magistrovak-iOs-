fba5448dd840165beac188b13055ae16d2210a14
Lucie Havrdová <havrdova1lucie@gmail.com>
2023-02-23 14:36:33 +0100

    spm projet init
    spm project init

---
+++ magistrovak/App/Feature/Sources/TabBar/TabBarView.swift
@@ -0,0 +1,23 @@
+import SwiftUI
+
+// MARK: - TabBar View
+
+public struct TabBarView: View {
+
+    public init() {}
+
+    public var body: some View {
+        VStack {
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+// MARK: - TabBar Preview
+
+struct TabBarView_Previews: PreviewProvider {
+    static var previews: some View {
+        TabBarView()
+    }
+}
---
+++ magistrovak/App/Magistrovak.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+import TabBar
+
+@main
+struct Magistrovak: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                TabBarView()
+            }
+        }
+    }
+}
