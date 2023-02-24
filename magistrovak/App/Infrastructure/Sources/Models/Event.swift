11105620e2eb4d1f4923313375deabba25f3ae3b
Lucie Havrdová <havrdova1lucie@gmail.com>
2023-02-24 17:52:19 +0100

    Event model

---
+++ magistrovak/App/Infrastructure/Sources/Models/Event.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+// MARK: - Event
+
+public struct Event: Equatable {
+    public let id: String
+    public let name: String
+    public let startTime: String
+    public let endTime: String
+    public let date: String
+}
--- magistrovak/App/Infrastructure/Sources/Models/Program.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-// MARK: - Program
-
-public struct Program: Equatable {
-    public let id: String
-    public let name: String
-    public let time: String
-}
